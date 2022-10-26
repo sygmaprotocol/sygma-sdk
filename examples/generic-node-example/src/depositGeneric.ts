c18781cf1fc4ee22d52142cddfb1feafa3a38e79
Nicolas Riquelme <nrriquelme@gmail.com>
2022-10-25 23:14:58 -0300

    example and scripts

--- examples/generic-node-example/src/depositGeneric.ts
+++ examples/generic-node-example/src/depositGeneric.ts
@@ -1,10 +1,10 @@
 import { ethers } from 'ethers'
-import { NonceManager } from '@ethersproject/experimental'
 import Chalk from 'chalk'
 import ColorsABI from './abis/colors-abi.json'
 import {
   colorsAddress,
   node1RpcUrl,
+  node2RpcUrl,
   bridgeAdmin
 } from './bridgeSetup'
 import { decodeColor } from './abis/decodeColor';
@@ -12,21 +12,20 @@ import { setupSygma } from './sygmaInstance'
 import { FeeDataResult } from '@buildwithsygma/sygma-sdk-core'
 
 const providerNode1 = new ethers.providers.JsonRpcProvider(node1RpcUrl);
-
-const walletNode1 = ethers.Wallet.fromMnemonic(
-  "black toward wish jar twin produce remember fluid always confirm bacon slush",
-  "m/44'/60'/0'/0/0",
-);
-
-const walletSignerNode1 = walletNode1.connect(providerNode1);
-
-const managedSignerNode1 = new NonceManager(walletSignerNode1);
+const providerNode2 = new ethers.providers.JsonRpcProvider(node2RpcUrl)
 
 const depositGeneric = async () => {
+  const account = (await providerNode1.listAccounts())[0]
+  const signer = providerNode1.getSigner(account)
+  const balanceAccount = (await signer.getBalance()).toString()
+
+  console.log(`${Chalk.keyword('orange')(`Balance of account ${account}: `)} ${Chalk.keyword('yellow')(balanceAccount)}`)
 
   const colorContractNode1 = new ethers.Contract(colorsAddress, ColorsABI.abi)
 
-  const colorArrayLength = await colorContractNode1.connect(managedSignerNode1).getColorsArrayLenght()
+  const colorArrayLength = await colorContractNode1.connect(signer).getColorsArrayLenght()
+
+  let depositDataArray: Array<{ color: string, depositData: string }>
 
   if (colorArrayLength.toNumber() !== 0) {
     const iterable = Array.from(Array(colorArrayLength.toNumber()).keys()).map(i => i)
@@ -34,62 +33,70 @@ const depositGeneric = async () => {
     let colors = []
 
     for await (let k of iterable) {
-      const color = await colorContractNode1.connect(managedSignerNode1).colorsArray(k)
+      const color = await colorContractNode1.connect(signer).colorsArray(k)
       const colorDecoded = decodeColor(color)
       console.log(`${Chalk.keyword('orange')('Color decoded')}: ${Chalk.hex(colorDecoded).bold(colorDecoded)}`)
       colors.push(colorDecoded)
     }
 
     const colorsFormated = colors.map(color => color.substr(1))
-    console.log("🚀 ~ file: depositGeneric.ts ~ line 42 ~ depositGeneric ~ colorsFormated", colorsFormated)
     const depositFunctionSignature = '0x103b854b'
     const colorsResourceId = '0x0000000000000000000000000000000000000000000000000000000000000500'
 
-    const depositDataFee = `0x${
-      // @ts-ignore-next-line
-      ethers.utils.hexZeroPad(100, 32).substr(2) +
-      // @ts-ignore-next-line
-      ethers.utils.hexZeroPad(bridgeAdmin.length, 32).substr(2) +
-      (await managedSignerNode1.getAddress()).substr(2)
-      }`
-
-    const sygmaConnected = await setupSygma(await managedSignerNode1.getAddress())
+    const sygmaConnected = await setupSygma(account)
 
     const basicFeeData = await sygmaConnected.fetchBasicFeeData({
       amount: '1000000',
       recipientAddress: bridgeAdmin
     })
-    console.log("🚀 ~ file: depositGeneric.ts ~ line 61 ~ depositGeneric ~ basicFeeData", basicFeeData)
 
     const colorsFormatedToHex = colorsFormated.map(color => ({ color: color, colorToHex: sygmaConnected.toHex(`0x${color}`, 32) }))
-    console.log("🚀 ~ file: depositGeneric.ts ~ line 64 ~ depositGeneric ~ colorFormatedToHex", colorsFormatedToHex)
 
-    const depositDataArray: Array<{color: string, depositData: string}> = colorsFormatedToHex.map(color => ({
+    depositDataArray = colorsFormatedToHex.map(color => ({
       color: color.color,
       depositData: sygmaConnected.createGenericDepositDataV1(
         depositFunctionSignature,
         colorsAddress,
         '2000000',
-        bridgeAdmin,
+        account,
         color.colorToHex,
         false
       )
     }))
-    console.log("🚀 ~ file: depositGeneric.ts ~ line 74 ~ depositGeneric ~ depositDateArray", depositDataArray)
 
-    for await (let {color, depositData} of depositDataArray) {
+    for await (let { color, depositData } of depositDataArray) {
       try {
         const depositTx = await sygmaConnected.depositGeneric(
           colorsResourceId,
           depositData,
           basicFeeData as FeeDataResult
         )
+        console.log(`${Chalk.keyword('orange')('Status of deposit')} ${Chalk.keyword('yellow')(depositTx?.status)}`)
         console.log(`${Chalk.keyword('red')('Color being sent:')} ${Chalk.hex(color).bold(color)}`)
-      } catch (e){
+      } catch (e) {
         console.log("Error on generic deposit", e)
       }
     }
   }
+
+  const signerDestinationChain = providerNode2.getSigner(account)
+
+  const filters = colorContractNode1.connect(signerDestinationChain).filters.setColorEvent()
+
+  const listener = async (color: string, ...rest: any) => {
+    const colorDecoded = decodeColor(color)
+    console.log(`${Chalk.keyword('green')('Color decoded on destination chain')}: ${Chalk.hex(colorDecoded).bold(colorDecoded)}`)
+    const lastColor = depositDataArray.findIndex(colorData => colorData.color === colorDecoded.substr(1))
+
+    if (lastColor === depositDataArray.length - 1) {
+      setTimeout(() => {
+        process.exit(0)
+      }, 5000)
+    }
+  }
+
+  colorContractNode1.connect(signerDestinationChain).on(filters, listener)
+
 }
 
 depositGeneric()
--- examples/generic-node-example/src/sygmaInstance.ts
+++ examples/generic-node-example/src/sygmaInstance.ts
@@ -7,7 +7,6 @@ import {
 const setupSygma = async (signerAddress: string): Promise<Sygma> => {
   const setup = { bridgeSetup }
   const sygma = new Sygma(setup)
-  console.log("🚀 ~ file: sygmaInstance.ts ~ line 10 ~ setupSygma ~ sygma", sygma)
   
   const sygmaConnected = await sygma.initializeConnectionRPC(signerAddress)
   return sygmaConnected
