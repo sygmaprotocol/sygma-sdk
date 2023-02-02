be916b02cf15253213728a27a25e150cd7fa58dd
Nikolay Topkaridi <enemycnt@gmail.com>
2023-02-02 23:46:31 +0600

    updated jest and jest setup

--- packages/sdk/.eslintrc.js
+++ packages/sdk/.eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
   extends: "../../.eslintrc.js",
   parserOptions: {
-    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    // project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
   },
 }
