60037ac0d6ec8e00bc255f87f394dcfef2248339
Nikolay Topkaridi <enemycnt@gmail.com>
2023-02-03 00:43:11 +0600

    removed comment

--- examples/substrate-react-example/vite.config.ts
+++ examples/substrate-react-example/vite.config.ts
@@ -13,9 +13,6 @@ export default defineConfig({
     },
   },
   define: {
-    // "process.env": process.env,
-    // // By default, Vite doesn't include shims for NodeJS/
-    // // necessary for segment analytics lib to work
     global: {},
   },
   resolve: {
