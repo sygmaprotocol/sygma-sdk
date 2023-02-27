808559dd269f6e2f3aa8d7ce63deaa7914755f5f
Nikolay Topkaridi <enemycnt@gmail.com>
2023-02-27 18:11:13 +0400

    changed to vite-plugin-node-polyfills in substrate example

--- examples/substrate-react-example/vite.config.ts
+++ examples/substrate-react-example/vite.config.ts
@@ -1,29 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   build: {
     commonjsOptions: {
       include: [/node_modules/],
-    },
-  },
-  define: {
-    global: {},
-  },
-  resolve: {
-    alias: {
-      process: "process/browser",
-      buffer: "buffer",
-      crypto: "crypto-browserify",
-      stream: "stream-browserify",
-      assert: "assert",
-      http: "stream-http",
-      https: "https-browserify",
-      os: "os-browserify",
-      url: "url",
-      util: "util/",
     },
   },
 });
