e998a78abbc5e3f978456f0017db38ad097f0544
Nikolay Topkaridi <enemycnt@gmail.com>
2023-04-25 01:05:39 +0400

    updated jsodcs

--- packages/sdk/src/chains/Substrate/utils/listenForEvent.ts
+++ packages/sdk/src/chains/Substrate/utils/listenForEvent.ts
@@ -5,11 +5,24 @@ import type { AnyJson } from '@polkadot/types-codec/types';
 /**
  * Listens for an event and calls a callback when it is triggered.
  *
+ * @example
+ * // Assuming you have a valid ApiPromise instance (api)
+ * // and a callback function (callback)
+ * await listenForEvent(api, "ProposalExecution", (data) => {
+ *   console.log("ProposalExecution", data);
+ *   const dataEvent = data as {
+ *     depositNonce: string;
+ *     dataHash: string;
+ *    };
+ *    console.log("ProposalExecution event is emmited. Transfer is finished")
+ *    console.log(dataEvent)
+ * });
+ *
  * @category Event handling
  * @param {ApiPromise} api - An ApiPromise instance.
  * @param {string} eventName - The name of the event to listen for.
  * @param {(data: AnyJson) => void} callback - The function to call when the event is triggered.
- * @returns {Promise<() => void) | undefined>}  A promise that resolves when the event is triggered.
+ * @returns {Promise<(() => void) | undefined>}  A promise that resolves when the event is triggered.
  */
 export const listenForEvent = async (
   api: ApiPromise,
@@ -23,6 +36,7 @@ export const listenForEvent = async (
       events.forEach(({ event: { data, method, section } }) => {
         if (section === 'sygmaBridge' && method === eventName) {
           callback(data.toHuman());
+          console.log(unsub);
           unsub();
         }
       });
