b7136de4f9df9fc189ec5f46005a0bfa4b6b6b1d
mpetrun5 <matija.petrunic@gmail.com>
2023-05-19 15:13:45 +0200

    Add erc20 transfer example

---
+++ examples/erc20/src/transfer.ts
@@ -0,0 +1,72 @@
+import {
+  Domain,
+  EVMAssetTransfer,
+  Environment,
+  Fungible,
+  Resource,
+  Transfer,
+} from "@buildwithsygma/sygma-sdk-core";
+import { Wallet, providers } from "ethers";
+
+export const GOERLI_CHAIN_ID = 5;
+export const SEPOLIA_CHAIN_ID = 11155111;
+export const ERC20_TOKEN_SYMBOL = "ERC20LRTest";
+
+export async function erc20Transfer(): Promise<void> {
+  const provider = new providers.JsonRpcProvider(
+    "https://rpc.goerli.eth.gateway.fm/"
+  );
+  const wallet = new Wallet(
+    "9574830b950b9ce5b66836e77ea18739258682ca08d4e65ca26b03dfe3742cf9",
+    provider
+  );
+  const assetTransfer = new EVMAssetTransfer();
+  await assetTransfer.init(provider, Environment.DEVNET);
+
+  const domains: Array<Domain> = assetTransfer.config.getDomains();
+  const resources: Array<Resource> = assetTransfer.config.getDomainResources();
+  const erc20Resource = resources.find(
+    (resource) => resource.symbol == ERC20_TOKEN_SYMBOL
+  );
+  if (!erc20Resource) {
+    throw new Error("Resource not found");
+  }
+  const goerli = domains.find((domain) => domain.chainId == GOERLI_CHAIN_ID);
+  if (!goerli) {
+    throw new Error("Network goerli not supported");
+  }
+  const sepolia = domains.find((domain) => domain.chainId == SEPOLIA_CHAIN_ID);
+  if (!sepolia) {
+    throw new Error("Network sepolia not supported");
+  }
+  const transfer: Transfer<Fungible> = {
+    sender: await wallet.getAddress(),
+    amount: {
+      // amount in wei
+      amount: "50",
+    },
+    from: goerli,
+    to: sepolia,
+    resource: erc20Resource,
+    recipient: await wallet.getAddress(),
+  };
+
+  const fee = await assetTransfer.getFee(transfer);
+  const approvals = await assetTransfer.buildApprovals(transfer, fee);
+  for (const approval of approvals) {
+    const response = await wallet.sendTransaction(
+      approval as providers.TransactionRequest
+    );
+    console.log("Sent approval with hash: " + response.hash);
+  }
+  const transferTx = await assetTransfer.buildTransferTransaction(
+    transfer,
+    fee
+  );
+  const response = await wallet.sendTransaction(
+    transferTx as providers.TransactionRequest
+  );
+  console.log("Sent transfer with hash: " + response.hash);
+}
+
+erc20Transfer().finally(() => {});
--- packages/sdk/src/chains/EVM/assetTransfer.ts
+++ packages/sdk/src/chains/EVM/assetTransfer.ts
@@ -171,7 +171,7 @@ export class EVMAssetTransfer {
         break;
       }
       default:
-        throw new Error(`Resource type ${transfer.resource.type} not supported by asset transfer`);
+        throw new Error(`Resource type not supported by asset transfer`);
     }
 
     return approvals;
@@ -212,7 +212,7 @@ export class EVMAssetTransfer {
         });
       }
       default:
-        throw new Error(`Resource type ${transfer.resource.type} not supported by asset transfer`);
+        throw new Error(`Resource type not supported by asset transfer`);
     }
   }
 
