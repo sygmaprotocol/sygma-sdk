700be9321245eb2cb3a02388943b756b6d137c18
mace <mak@chainsafe.io>
2023-06-21 14:25:33 +0200

    fix: local example (#273)
    missing commit
    
    Co-authored-by: MakMuftic <makmuftic@chainsafe.io>

--- examples/local-fungible-transfer/src/transfer.ts
+++ examples/local-fungible-transfer/src/transfer.ts
@@ -28,7 +28,7 @@ const EVM2_RPC_URL = "http://127.0.0.1:8547"
 const SUBSTRATE_RPC_URL = "ws://127.0.0.1:9944"
 
 const ALICE_MNEMONIC =
-  "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice";
+    "bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice";
 const wsProvider = new WsProvider(
     SUBSTRATE_RPC_URL
 );
@@ -52,7 +52,7 @@ export async function fungibleTransferFromEVM(): Promise<void> {
   const resources = assetTransfer.config.getDomainResources();
 
   const erc20Resource = resources.find(
-    (resource) => resource.symbol == ERC20_TOKEN_SYMBOL
+      (resource) => resource.symbol == ERC20_TOKEN_SYMBOL
   );
   if (!erc20Resource) {
     throw new Error("Resource not found");
@@ -62,7 +62,7 @@ export async function fungibleTransferFromEVM(): Promise<void> {
     throw new Error("Network evm1 not supported");
   }
   const substrateNetwork = domains.find(
-    (domain) => domain.chainId == SUBSTRATE_CHAIN_ID
+      (domain) => domain.chainId == SUBSTRATE_CHAIN_ID
   );
   if (!substrateNetwork) {
     throw new Error("Network substrate not supported");
@@ -77,7 +77,7 @@ export async function fungibleTransferFromEVM(): Promise<void> {
   const destinationBalanceBefore = BigNumber.from(b.balance.split(',').join(''))
 
   const sourceErc20LR18Contract = new ERC20PresetMinterPauser__factory(
-    wallet as any
+      wallet as any
   ).attach(ERC20_TOKEN_ADDRESS);
   const balanceBefore = await sourceErc20LR18Contract.balanceOf(sender)
 
@@ -90,28 +90,28 @@ export async function fungibleTransferFromEVM(): Promise<void> {
     sender: sender,
     amount: {
       // amount in wei
-      amount: "500000000000",
+      amount: "5000000000000000000",
     },
     from: evm1Network,
     to: substrateNetwork,
     resource: erc20Resource,
-    recipient: EVM_DESTINATION_ADDRESS,
+    recipient: account.address,
   };
 
   const fee = await assetTransfer.getFee(transfer);
   const approvals = await assetTransfer.buildApprovals(transfer, fee);
   for (const approval of approvals) {
     const response = await wallet.sendTransaction(
-      approval as providers.TransactionRequest
+        approval as providers.TransactionRequest
     );
     console.log("Sent approval with hash: " + response.hash);
   }
   const transferTx = await assetTransfer.buildTransferTransaction(
-    transfer,
-    fee
+      transfer,
+      fee
   );
   const response = await wallet.sendTransaction(
-    transferTx as providers.TransactionRequest
+      transferTx as providers.TransactionRequest
   );
   console.log("Sent transfer with hash: " + response.hash);
 
@@ -156,7 +156,7 @@ export async function fungibleTransferFromSubstrate(): Promise<void> {
   const destinationBalanceBefore = await destinationErc20LRContract.balanceOf(EVM_DESTINATION_ADDRESS)
 
   const sourceBalanceBefore = (
-    await api.query.assets.account(2000, account.address)
+      await api.query.assets.account(2000, account.address)
   ).toHuman() as { balance: string };
   console.log(`Transferring 5 tokens from substrate to evm1.`);
   console.log(`Sender (Alice): ${account.address}`);
@@ -165,16 +165,16 @@ export async function fungibleTransferFromSubstrate(): Promise<void> {
 
   const assetTransfer = new SubstrateAssetTransfer();
   await assetTransfer.init(
-    api,
-    SubstrateParachain.LOCAL,
-    Environment.LOCAL
+      api,
+      SubstrateParachain.LOCAL,
+      Environment.LOCAL
   );
 
   const domains = assetTransfer.config.getDomains();
   const resources = assetTransfer.config.getDomainResources();
 
   const erc20Resource = resources.find(
-    (resource) => resource.resourceId == RESOURCE_ID
+      (resource) => resource.resourceId == RESOURCE_ID
   );
   if (!erc20Resource) {
     throw new Error("Resource not found");
@@ -184,7 +184,7 @@ export async function fungibleTransferFromSubstrate(): Promise<void> {
     throw new Error("Network goerli not supported");
   }
   const substrateNetwork = domains.find(
-    (domain) => domain.chainId == EVM1_CHAIN_ID
+      (domain) => domain.chainId == EVM1_CHAIN_ID
   );
   if (!substrateNetwork) {
     throw new Error("Network sepolia not supported");
@@ -210,11 +210,11 @@ export async function fungibleTransferFromSubstrate(): Promise<void> {
 
     if (status.isInBlock) {
       console.log(
-        `Transaction included at blockHash ${status.asInBlock.toString()}`
+          `Transaction included at blockHash ${status.asInBlock.toString()}`
       );
     } else if (status.isFinalized) {
       console.log(
-        `Transaction finalized at blockHash ${status.asFinalized.toString()}`
+          `Transaction finalized at blockHash ${status.asFinalized.toString()}`
       );
       unsub();
     }
@@ -242,7 +242,7 @@ export async function fungibleTransferFromSubstrate(): Promise<void> {
   }
 
   const balanceAfter = (
-    await api.query.assets.account(2000, account.address)
+      await api.query.assets.account(2000, account.address)
   ).toHuman() as { balance: string };
 
   console.log(`Alice token balance on substrate network after: ${balanceAfter.balance}`);
@@ -264,4 +264,3 @@ switch (process.argv[2]) {
   default:
     console.log("example not supported")
 }
-
