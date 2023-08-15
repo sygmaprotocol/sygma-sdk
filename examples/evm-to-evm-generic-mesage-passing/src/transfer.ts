4e5c8154f5aaa85cd821ea84276aa74c2240983b
FSM1 <myankelev@gmail.com>
2023-08-15 12:46:10 +0200

    update example

--- examples/evm-to-evm-generic-mesage-passing/src/transfer.ts
+++ examples/evm-to-evm-generic-mesage-passing/src/transfer.ts
@@ -20,26 +20,53 @@ const RESOURCE_ID =
 const EXECUTE_CONTRACT_ADDRESS = "0xdFA5621F95675D37248bAc9e536Aab4D86766663";
 const EXECUTE_FUNCTION_SIGNATURE = "0xa271ced2";
 const MAX_FEE = "3000000";
+const sourceProvider = new providers.JsonRpcProvider(
+  "https://gateway.tenderly.co/public/sepolia"
+);
+const destinationProvider = new providers.JsonRpcProvider(
+  "https://rpc.goerli.eth.gateway.fm/"
+);
+const storageContract = Storage__factory.connect(
+  EXECUTE_CONTRACT_ADDRESS,
+  destinationProvider
+);
+const wallet = new Wallet(privateKey ?? "", sourceProvider);
+
+const fetchAfterValue = async (): Promise<BigNumber> =>
+  await storageContract.retrieve(await wallet.getAddress());
 
 const sleep = (ms: number): Promise<void> =>
   new Promise((r) => setTimeout(r, ms));
 
+const waitUntilBridged = async (
+  valueBefore: BigNumber,
+  intervalDuration: number = 15000,
+  attempts: number = 8,
+): Promise<void> => {
+  let i = 0;
+  let contractValueAfter: BigNumber;
+  for (; ;) {
+    await sleep(intervalDuration);
+    contractValueAfter = await fetchAfterValue();
+    if (!contractValueAfter.eq(valueBefore)) {
+      console.log("Transaction successfully bridged.");
+      console.log(
+        `Value after update: ${new Date(
+          contractValueAfter.toNumber()
+        ).toString()}`
+      );
+      break;
+    }
+    i++;
+    if (i > attempts) {
+      // transaction should have been bridged already
+      console.log("transaction is taking too much time to bridge!");
+      break;
+    }
+  }
+};
+
 export async function genericMessage(): Promise<void> {
-  const sourceProvider = new providers.JsonRpcProvider(
-    "https://gateway.tenderly.co/public/sepolia"
-  );
-
-  const destinationProvider = new providers.JsonRpcProvider(
-    "https://rpc.goerli.eth.gateway.fm/"
-  );
-
-  const wallet = new Wallet(privateKey ?? "", sourceProvider);
-
-  const storageContract = Storage__factory.connect(
-    EXECUTE_CONTRACT_ADDRESS,
-    destinationProvider
-  );
-
   const contractValueBefore = await storageContract.retrieve(
     await wallet.getAddress()
   );
@@ -76,27 +103,7 @@ export async function genericMessage(): Promise<void> {
 
   console.log("Waiting for relayers to bridge transaction...");
 
-  let i = 0;
-  let contractValueAfter: BigNumber;
-  for (; ;) {
-    await sleep(15000);
-    contractValueAfter = await storageContract.retrieve(
-      await wallet.getAddress()
-    );
-    if (!contractValueAfter.eq(contractValueBefore)) {
-      console.log("Transaction successfully bridged.");
-      break;
-    }
-    i++;
-    if (i > 8) {
-      // transaction should have been bridged already
-      console.log("transaction is taking too much time to bridge!");
-      break;
-    }
-  }
-  console.log(
-    `Value after update: ${new Date(contractValueAfter.toNumber()).toString()}`
-  );
+  await waitUntilBridged(contractValueBefore);
 }
 
 genericMessage().finally(() => { });
