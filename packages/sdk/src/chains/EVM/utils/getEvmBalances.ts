58a75a05aafd66b4e7a2e8ce07d418720e177cf6
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2024-01-16 16:40:48 -0300

    feat: small refactor to get balances on Evm context (#324)
    * feat: small refactor to get balances on Evm context
    
    * feat: remove log
    
    * chore: pr review

--- packages/sdk/src/chains/EVM/utils/getEvmBalances.ts
+++ packages/sdk/src/chains/EVM/utils/getEvmBalances.ts
@@ -12,7 +12,15 @@ export const getEvmHandlerBalance = async (
     return BigInt((await provider.getBalance(handlerAddress)).toString());
   } else {
     const tokenAddress = resource.address;
-    const erc20Contract = ERC20__factory.connect(tokenAddress, provider);
-    return BigInt((await erc20Contract.balanceOf(handlerAddress)).toString());
+    return await getEvmErc20Balance(handlerAddress, tokenAddress, provider);
   }
+};
+
+export const getEvmErc20Balance = async (
+  address: string,
+  tokenAddress: string,
+  provider: JsonRpcProvider,
+): Promise<bigint> => {
+  const erc20Contract = ERC20__factory.connect(tokenAddress, provider);
+  return BigInt((await erc20Contract.balanceOf(address)).toString());
 };
