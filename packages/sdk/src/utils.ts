b056e1502f6da2f86a9f82d059d1a6e3520b72af
BeroBurny <bero4net@gmail.com>
2024-02-13 15:01:37 +0100

    fix: getTransferStatusData

--- packages/sdk/src/utils.ts
+++ packages/sdk/src/utils.ts
@@ -24,29 +24,48 @@ export function getFeeOracleBaseURL(environment?: Environment): string {
 export async function getTransferStatusData(
   environment: Environment,
   txHash: string,
+): Promise<TransferStatusResponse[]>;
+export async function getTransferStatusData(
+  environment: Environment,
+  txHash: string,
+  domainId: string,
+): Promise<TransferStatusResponse>;
+export async function getTransferStatusData(
+  environment: Environment,
+  txHash: string,
   domainId?: string,
-): Promise<TransferStatusResponse> {
+): Promise<TransferStatusResponse | TransferStatusResponse[]> {
   let url: string;
   let explorerUrl: string;
 
   if (environment === Environment.TESTNET) {
-    url = `${IndexerUrl.TESTNET}/api/transfers/txHash/${txHash}${
-      domainId ? `?domainId=${domainId}` : ''
-    }`;
+    url = `${IndexerUrl.TESTNET}/api/transfers/txHash/${txHash}`;
     explorerUrl = `${ExplorerUrl.TESTNET}/transfer/${txHash}`;
   } else if (environment === Environment.MAINNET) {
-    url = `${IndexerUrl.MAINNET}/api/transfers/txHash/${txHash}${
-      domainId ? `?domainId=${domainId}` : ''
-    }`;
+    url = `${IndexerUrl.MAINNET}/api/transfers/txHash/${txHash}`;
     explorerUrl = `${ExplorerUrl.MAINNET}/transfer/${txHash}`;
   } else {
     throw new Error('Invalid environment');
   }
 
   const response = await fetch(url);
-  const data = (await response.json()) as Record<string, unknown> & { status: TransferStatus };
-  return {
-    status: data.status,
+  const data = (await response.json()) as (Record<string, unknown> & {
+    status: TransferStatus;
+    toDomainId: number;
+  })[];
+
+  if (domainId) {
+    const record = data.find(data => data.toDomainId === Number(domainId));
+    if (!record)
+      throw new Error(`Transfer with txHash: ${txHash} for domain id: ${domainId} not found.`);
+
+    return {
+      status: record.status,
+      explorerUrl,
+    };
+  }
+  return data.map(record => ({
+    status: record.status,
     explorerUrl,
-  };
+  }));
 }
