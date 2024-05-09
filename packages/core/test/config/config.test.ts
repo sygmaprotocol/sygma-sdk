d2565ac475a1baefa0a7b0408b171c003e8ea974
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2024-05-09 19:14:38 +0200

    Migrate and add new tests

--- packages/core/test/config/config.test.ts
+++ packages/core/test/config/config.test.ts
@@ -18,7 +18,7 @@ describe('Config', () => {
   });
 
   it('Should successfully initialize config for the corresponding environment', async function () {
-    await config.init(Environment.DEVNET);
+    await config.init({ environment: Environment.DEVNET });
     expect(config.configuration).toEqual(mockedDevnetConfig);
   });
 
@@ -32,13 +32,13 @@ describe('Config', () => {
 
     const conf = new Config();
     const expectedError = new Error('Configuration unavailable or uninitialized.');
-    await conf.init(Environment.MAINNET);
+    await conf.init({ environment: Environment.MAINNET });
 
     expect(() => conf.configuration).toThrow(expectedError);
   });
 
   it('Should successfully return all domains from config', async function () {
-    await config.init(Environment.DEVNET);
+    await config.init({ environment: Environment.DEVNET });
 
     const domainConfig = config.getDomainConfig(111);
 
@@ -49,13 +49,13 @@ describe('Config', () => {
     fetchMock.mockIf(ConfigUrl.TESTNET.toString(), JSON.stringify({ domains: [] }));
 
     const expectedError = new Error('Config for the provided domain is not setup.');
-    await config.init(Environment.TESTNET);
+    await config.init({ environment: Environment.TESTNET });
 
     expect(() => config.getDomainConfig(115)).toThrow(expectedError);
   });
 
   it('Should successfully return all resources for domain', async function () {
-    await config.init(Environment.DEVNET);
+    await config.init({ environment: Environment.DEVNET });
 
     const resources = config.getDomainResources(111);
 
@@ -63,7 +63,7 @@ describe('Config', () => {
   });
 
   it('Should successfully get all supported domains from config', async function () {
-    await config.init(Environment.DEVNET);
+    await config.init({ environment: Environment.DEVNET });
 
     const domains = config.getDomains();
 
@@ -80,12 +80,11 @@ describe('Config', () => {
   });
 
   it('Should fetch all configurations by default', async function () {
-    await config.init();
-    config.setEnvironment(111);
+    await config.init({ environment: Environment.DEVNET, source: 111 });
     expect(config.environment).toEqual(Environment.DEVNET);
-    config.setEnvironment(211);
+    await config.init({ environment: Environment.TESTNET, source: 211 });
     expect(config.environment).toEqual(Environment.TESTNET);
-    config.setEnvironment(311);
+    await config.init({ environment: Environment.MAINNET, source: 311 });
     expect(config.environment).toEqual(Environment.MAINNET);
   });
 });
