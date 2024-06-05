4009f08c07e4b8e75b1b0ecc7c68ab76b3103190
haochizzle <tim.hch@gmail.com>
2024-06-05 18:35:19 +0800

    change transfer.ts destination chain from cronos to base-sepolia

--- examples/evm-to-evm-generic-mesage-passing/src/transfer.ts
+++ examples/evm-to-evm-generic-mesage-passing/src/transfer.ts
@@ -19,21 +19,22 @@ if (!privateKey) {
 const getStatus = async (
   txHash: string
 ): Promise<TransferStatusResponse[]> => {
-  const data = await getTransferStatusData(Environment.TESTNET, txHash);
-  return data as TransferStatusResponse[];
+    const data = await getTransferStatusData(Environment.TESTNET, txHash);
+    return data as TransferStatusResponse[];
 };
 
-const DESTINATION_CHAIN_ID = 338; // Cronos
+const DESTINATION_CHAIN_ID = 84532; // base sepolia
 const RESOURCE_ID =
   "0x0000000000000000000000000000000000000000000000000000000000000500"; // Generic Message Handler
-const EXECUTE_CONTRACT_ADDRESS = "0xcb9eb2b2abbd51945a82f77e789c26720b3835bf";
+const EXECUTE_CONTRACT_ADDRESS = "0x669f52487ffa6f9abf722082f735537a98ec0e4b";
 const EXECUTE_FUNCTION_SIGNATURE = "0xa271ced2";
-const MAX_FEE = "6000000";
-const CRONOS_RPC_URL = process.env.CRONOS_RPC_URL || "https://evm-t3.cronos.org";
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://gateway.tenderly.co/public/sepolia";
+const MAX_FEE = "3000000";
+const BASE_SEPOLIA_RPC_URL = process.env.BASE_SEPOLIA_RPC_URL || "https://sepolia.base.org"
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://gateway.tenderly.co/public/sepolia"
 
 const sourceProvider = new providers.JsonRpcProvider(SEPOLIA_RPC_URL);
-const destinationProvider = new providers.JsonRpcProvider(CRONOS_RPC_URL);
+const destinationProvider = new providers.JsonRpcProvider(BASE_SEPOLIA_RPC_URL
+);
 const storageContract = Storage__factory.connect(
   EXECUTE_CONTRACT_ADDRESS,
   destinationProvider
@@ -58,13 +59,17 @@ const waitUntilBridged = async (
     contractValueAfter = await fetchAfterValue();
     if (!contractValueAfter.eq(valueBefore)) {
       console.log("Transaction successfully bridged.");
-      console.log(`Value after update: ${contractValueAfter.toString()}`);
+      console.log(
+        `Value after update: ${new Date(
+          contractValueAfter.toNumber()
+        ).toString()}`
+      );
       break;
     }
     i++;
     if (i > attempts) {
       // transaction should have been bridged already
-      console.log("Transaction is taking too much time to bridge!");
+      console.log("transaction is taking too much time to bridge!");
       break;
     }
   }
@@ -74,14 +79,15 @@ export async function genericMessage(): Promise<void> {
   const contractValueBefore = await storageContract.retrieve(
     await wallet.getAddress()
   );
-  console.log(`Value before update: ${contractValueBefore.toString()}`);
+  console.log(
+    `Value before update: ${new Date(
+      contractValueBefore.toNumber()
+    ).toString()}`
+  );
   const messageTransfer = new EVMGenericMessageTransfer();
   await messageTransfer.init(sourceProvider, Environment.TESTNET);
 
-  const EXECUTION_DATA = utils.defaultAbiCoder.encode(
-    ["address", "uint256"],
-    [await wallet.getAddress(), Date.now()]
-  );
+  const EXECUTION_DATA = utils.defaultAbiCoder.encode(["uint"], [Date.now()]);
 
   const transfer = messageTransfer.createGenericMessageTransfer(
     await wallet.getAddress(),
