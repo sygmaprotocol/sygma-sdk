0bd7158a5020d7557227bfd90ea588e4259c1d33
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2024-06-18 10:20:40 -0400

    chore: better method naming + btc resource definition on core package

--- packages/btc/src/base-transfer.ts
+++ packages/btc/src/base-transfer.ts
@@ -1,16 +1,34 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
+import { BitcoinResource } from "@buildwithsygma/core/src";
+import { Config } from "@buildwithsygma/core/types";
+
 type BitcoinTransferRequest = {
   destinationAddress: string;
   amount: bigint;
 };
 
-export function createUriEncodedUtxoRequest(): BaseTransfer {
+export function createTransferRequest(): BaseTransfer {
   throw new Error("Method not implemented");
 }
 
 export abstract class BaseTransfer {
-  constructor(transfer: BitcoinTransferRequest) {}
+  constructor(transfer: BitcoinTransferRequest, config: Config) {}
+
+  private findResource(
+    resources: BitcoinResource[],
+    resourceIdentifier: string | BitcoinResource,
+  ): BitcoinResource | undefined {
+    throw new Error("Method not implemented");
+  }
+
+  /**
+   * Set resource to be transferred.
+   * @param {SubstrateResource} resource
+   */
+  setResource(resource: BitcoinResource): void {
+    throw new Error("Method not implemented");
+  }
 
   getUriEncodedUtxoRequest(btcTransferRequest: BitcoinTransferRequest): string {
     throw new Error("Method not implemented");
