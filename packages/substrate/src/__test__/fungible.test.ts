esculapesa/bridge

add substrate balance check

packages/substrate/src/__test__/fungible.test.ts
@@ -1,5 +1,6 @@
 import { FeeHandlerType } from '@buildwithsygma/core';
-import type { ApiPromise, SubmittableResult } from '@polkadot/api';
+import type { SubmittableResult } from '@polkadot/api';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 import type { SubmittableExtrinsic } from '@polkadot/api-base/types';
 import { BN } from '@polkadot/util';
 
@@ -8,19 +9,57 @@ import { createSubstrateFungibleAssetTransfer } from '../fungible.js';
 import { deposit, getBasicFee, getFeeHandler, getPercentageFee } from '../utils/index.js';
 
 jest.mock('../utils/index.js');
-
-describe('SubstrateFungibleAssetTransfer', () => {
-  const transferRequest: SubstrateAssetTransferRequest = {
-    sourceDomain: 5, // Substrate
-    destinationDomain: 1337, // Ethereum
-    sourceNetworkProvider: {} as ApiPromise,
-    resource: '0x0000000000000000000000000000000000000000000000000000000000000300',
-    amount: BigInt(1000),
-    destinationAddress: '0x98729c03c4D5e820F5e8c45558ae07aE63F97461',
+jest.mock('@polkadot/api', () => {
+  const originalModule = jest.requireActual('@polkadot/api');
+  const mockBalance = {
+    data: {
+      free: '500',
+      reserved: '5000',
+      miscFrozen: '5000',
+      feeFrozen: '5000',
+    },
+  };
+  const mockChainProperties = {
+    tokenDecimals: ['12'],
+    tokenSymbol: ['DOT'],
   };
 
-  beforeEach(() => {
+  return {
+    ...originalModule,
+    ApiPromise: {
+      create: jest.fn().mockResolvedValue({
+        query: {
+          system: {
+            account: jest.fn().mockResolvedValue(mockBalance),
+          },
+        },
+        registry: {
+          getChainProperties: jest.fn().mockReturnValue(mockChainProperties),
+        },
+      }),
+    },
+  };
+});
+
+const RHALA_RPC_URL = 'wss://rhala-node.phala.network/ws';
+const wsProvider = new WsProvider(RHALA_RPC_URL);
+
+describe('SubstrateFungibleAssetTransfer', () => {
+  let api: jest.Mocked<ApiPromise>;
+  let transferRequest: SubstrateAssetTransferRequest;
+
+  beforeEach(async () => {
     jest.clearAllMocks();
+    api = (await ApiPromise.create({ provider: wsProvider })) as jest.Mocked<ApiPromise>;
+    transferRequest = {
+      sourceDomain: 5, // Substrate
+      destinationDomain: 1337, // Ethereum
+      sourceNetworkProvider: api,
+      resource: '0x0000000000000000000000000000000000000000000000000000000000000300',
+      amount: BigInt(1000),
+      destinationAddress: '0x98729c03c4D5e820F5e8c45558ae07aE63F97461',
+      senderAddress: '5E75Q88u1Hw2VouCiRWoEfKXJMFtqLSUzhqzsH6yWjhd8cem',
+    };
   });
 
   test('should validate Substrate address', async () => {
@@ -81,5 +120,30 @@ describe('SubstrateFungibleAssetTransfer', () => {
     const transfer = await createSubstrateFungibleAssetTransfer(transferRequest);
     const tx = await transfer.getTransferTransaction();
     expect(tx).toBeDefined();
+  });
+
+  test('should return ERROR - when Amount is less than transfer fee', async () => {
+    (getFeeHandler as jest.Mock).mockResolvedValue(FeeHandlerType.BASIC);
+    (getBasicFee as jest.Mock).mockResolvedValue({ fee: new BN(2000), type: FeeHandlerType.BASIC });
+
+    const transfer = await createSubstrateFungibleAssetTransfer(transferRequest);
+    await expect(transfer.getTransferTransaction()).rejects.toThrow(
+      'Transfer amount should be higher than transfer fee',
+    );
+  });
+
+  test('should return ERROR - when account balance is not sufficient', async () => {
+    (getFeeHandler as jest.Mock).mockResolvedValue(FeeHandlerType.BASIC);
+    (getBasicFee as jest.Mock).mockResolvedValue({ fee: new BN(100), type: FeeHandlerType.BASIC });
+
+    const insufficientBalanceRequest = {
+      ...transferRequest,
+      amount: BigInt(1000), // Amount set to trigger insufficient balance
+    };
+
+    const transfer = await createSubstrateFungibleAssetTransfer(insufficientBalanceRequest);
+    await expect(transfer.getTransferTransaction()).rejects.toThrow(
+      'Insufficient balance to perform the Transaction',
+    );
   });
 });

sygmaprotocol/sygma-sdk

packages/substrate/src/__test__/fungible.test.ts
@@ -1,5 +1,6 @@
 import { FeeHandlerType } from '@buildwithsygma/core';
-import type { ApiPromise, SubmittableResult } from '@polkadot/api';
+import type { SubmittableResult } from '@polkadot/api';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 import type { SubmittableExtrinsic } from '@polkadot/api-base/types';
 import { BN } from '@polkadot/util';
 
@@ -8,20 +9,58 @@ import { createSubstrateFungibleAssetTransfer } from '../fungible.js';
 import { deposit, getBasicFee, getFeeHandler, getPercentageFee } from '../utils/index.js';
 
 jest.mock('../utils/index.js');
-
-describe('SubstrateFungibleAssetTransfer', () => {
-  const transferRequest: SubstrateAssetTransferRequest = {
-    sourceAddress: '',
-    source: 5, // Substrate
-    destination: 1337, // Ethereum
-    sourceNetworkProvider: {} as ApiPromise,
-    resource: '0x0000000000000000000000000000000000000000000000000000000000000300',
-    amount: BigInt(1000),
-    destinationAddress: '0x98729c03c4D5e820F5e8c45558ae07aE63F97461',
+jest.mock('@polkadot/api', () => {
+  const originalModule = jest.requireActual('@polkadot/api');
+  const mockBalance = {
+    data: {
+      free: '500',
+      reserved: '5000',
+      miscFrozen: '5000',
+      feeFrozen: '5000',
+    },
+  };
+  const mockChainProperties = {
+    tokenDecimals: ['12'],
+    tokenSymbol: ['DOT'],
   };
 
-  beforeEach(() => {
+  return {
+    ...originalModule,
+    ApiPromise: {
+      create: jest.fn().mockResolvedValue({
+        query: {
+          system: {
+            account: jest.fn().mockResolvedValue(mockBalance),
+          },
+        },
+        registry: {
+          getChainProperties: jest.fn().mockReturnValue(mockChainProperties),
+        },
+      }),
+    },
+  };
+});
+
+const RHALA_RPC_URL = 'wss://rhala-node.phala.network/ws';
+const wsProvider = new WsProvider(RHALA_RPC_URL);
+
+describe('SubstrateFungibleAssetTransfer', () => {
+  let api: jest.Mocked<ApiPromise>;
+  let transferRequest: SubstrateAssetTransferRequest;
+
+  beforeEach(async () => {
     jest.clearAllMocks();
+    api = (await ApiPromise.create({ provider: wsProvider })) as jest.Mocked<ApiPromise>;
+    transferRequest = {
+      sourceAddress: '',
+      source: 5, // Substrate
+      destination: 1337, // Ethereum
+      sourceNetworkProvider: api,
+      resource: '0x0000000000000000000000000000000000000000000000000000000000000300',
+      amount: BigInt(1000),
+      destinationAddress: '0x98729c03c4D5e820F5e8c45558ae07aE63F97461',
+      senderAddress: '5E75Q88u1Hw2VouCiRWoEfKXJMFtqLSUzhqzsH6yWjhd8cem',
+    };
   });
 
   test('should validate Substrate address', async () => {
@@ -82,5 +121,30 @@ describe('SubstrateFungibleAssetTransfer', () => {
     const transfer = await createSubstrateFungibleAssetTransfer(transferRequest);
     const tx = await transfer.getTransferTransaction();
     expect(tx).toBeDefined();
+  });
+
+  test('should return ERROR - when Amount is less than transfer fee', async () => {
+    (getFeeHandler as jest.Mock).mockResolvedValue(FeeHandlerType.BASIC);
+    (getBasicFee as jest.Mock).mockResolvedValue({ fee: new BN(2000), type: FeeHandlerType.BASIC });
+
+    const transfer = await createSubstrateFungibleAssetTransfer(transferRequest);
+    await expect(transfer.getTransferTransaction()).rejects.toThrow(
+      'Transfer amount should be higher than transfer fee',
+    );
+  });
+
+  test('should return ERROR - when account balance is not sufficient', async () => {
+    (getFeeHandler as jest.Mock).mockResolvedValue(FeeHandlerType.BASIC);
+    (getBasicFee as jest.Mock).mockResolvedValue({ fee: new BN(100), type: FeeHandlerType.BASIC });
+
+    const insufficientBalanceRequest = {
+      ...transferRequest,
+      amount: BigInt(1000), // Amount set to trigger insufficient balance
+    };
+
+    const transfer = await createSubstrateFungibleAssetTransfer(insufficientBalanceRequest);
+    await expect(transfer.getTransferTransaction()).rejects.toThrow(
+      'Insufficient balance to perform the Transaction',
+    );
   });
 });