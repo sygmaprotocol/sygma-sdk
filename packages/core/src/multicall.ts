347a1ac73d65a2cf2199bbcd4db4522461d79502
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2024-09-10 10:51:42 -0300

    chore: error or file name

--- packages/core/src/multicall.ts
+++ packages/core/src/multicall.ts
@@ -6,7 +6,7 @@ import {
 import { Web3Provider } from '@ethersproject/providers';
 import { Contract, utils } from 'ethers';
 
-import { MulticallAbi } from './multicallAbi.js';
+import { MulticallAbi } from './multiCallAbi.js';
 import type { Eip1193Provider, FeeHandlerType, RouteIndexerType } from './types.js';
 
 enum MulticallDeployedNetworks {
